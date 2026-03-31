f9f0d6ed2702380f71599a2ab61240e3bc7441b4
Baryonics <benedikt.leicher@gmail.com>
2026-03-31 11:28:49 +0200

    Added more error handling. Removed read_size_from_file for redundancy

--- source/centipede/util/error_types.hpp
+++ source/centipede/util/error_types.hpp
@@ -28,8 +28,10 @@ namespace centipede
         analysis_global_negative_definite,
         analysis_factor_matrix_zero, //!< Global factor matrix is zero matrix.
         analysis_rhs_vector_zero,    //!< Global right-hand-side vector is zero vector.
-        reader_file_fail_to_open,    //!< Input File failed to be open.
+        reader_file_fail_to_open,    //!< Input file failed to be open.
         reader_file_fail_to_read,    //!< Input file failed to read
+        reader_uninitialized,        //!< Reader is not initialized.
+        reader_buffer_overflow,      //!< Buffer size is too small for a new entry occurs. See @ref reader::Binary.
     };
 
 } // namespace centipede
