eee41c6ce10552d3dc7e22e4824a975676be6337
Baryonics <benedikt.leicher@gmail.com>
2026-05-03 13:21:39 +0200

    More Unittests!

--- test/unit_tests/test_binary_reader.cpp
+++ test/unit_tests/test_binary_reader.cpp
@@ -7,7 +7,6 @@
 #include <fstream>
 #include <gtest/gtest.h>
 #include <ios>
-#include <istream>
 #include <iterator>
 #include <ranges>
 #include <string>
@@ -55,15 +54,43 @@ namespace centipede::test
         EXPECT_EQ(error.error(), ErrorCode::reader_file_fail_to_open);
     }
 
-    TEST(reader, file_invalid_size)
+    TEST(reader, not_initialized)
     {
-        auto file_name = std::string{ "file_invalid_size.bin" };
+        auto file_name = std::string{ "not_init.bin" };
+        auto file = std::ofstream{ file_name, std::ios::out | std::ios::binary | std::ios::trunc };
+        file.close();
+        auto reader = Binary{ Config{ .in_filename = file_name } };
+        auto read_err = reader.read_one_entry();
+        EXPECT_FALSE(read_err);
+        EXPECT_EQ(read_err.error(), ErrorCode::reader_uninitialized);
+    }
+
+    TEST(reader, file_invalid_idx_size)
+    {
+        auto file_name = std::string{ "file_invalid_idx_size.bin" };
         auto file = std::ofstream{ file_name, std::ios::out | std::ios::binary | std::ios::trunc };
         constexpr auto declared_entry_size = uint32_t{ 2 };
         constexpr auto dummy_data = uint32_t{ 1 };
         // NOLINTBEGIN (cppcoreguidelines-pro-type-reinterpret-cast)
         file.write(reinterpret_cast<const char*>(&declared_entry_size), sizeof(declared_entry_size));
         file.write(reinterpret_cast<const char*>(&dummy_data), sizeof(dummy_data));
+        // NOLINTEND (cppcoreguidelines-pro-type-reinterpret-cast)
+        file.close();
+        auto reader = Binary{ Config{ .in_filename = file_name } };
+        auto init_err = reader.init();
+        EXPECT_TRUE(init_err);
+        auto read_err = reader.read_one_entry();
+        EXPECT_FALSE(read_err);
+        EXPECT_EQ(read_err.error(), ErrorCode::reader_file_fail_to_read);
+    }
+
+    TEST(reader, file_invalid_val_size)
+    {
+        auto file_name = std::string{ "file_invalid_val_size.bin" };
+        auto file = std::ofstream{ file_name, std::ios::out | std::ios::binary | std::ios::trunc };
+        constexpr auto declared_entry_size = uint32_t{ 2 };
+        // NOLINTBEGIN (cppcoreguidelines-pro-type-reinterpret-cast)
+        file.write(reinterpret_cast<const char*>(&declared_entry_size), sizeof(declared_entry_size));
         // NOLINTEND (cppcoreguidelines-pro-type-reinterpret-cast)
         file.close();
         auto reader = Binary{ Config{ .in_filename = file_name } };
@@ -294,5 +321,21 @@ namespace centipede::test
         EXPECT_FALSE(read_err);
         EXPECT_EQ(read_err.error(), ErrorCode::reader_file_fail_to_read);
         reader.close();
+    }
+
+    TEST(reader, file_resize_buffer)
+    {
+        auto file_name = std::string{ "file_resize_buffer.bin" };
+        auto file = std::ofstream{ file_name, std::ios::out | std::ios::binary | std::ios::trunc };
+        auto output_buffer = Binary::RawBufferType{ { uint32_t{ 0 } }, { 0.F } };
+        fill_buffer(output_buffer, valid_measurement, valid_locals_data, valid_sigma, valid_globals_data);
+        fill_buffer(output_buffer, valid_measurement, valid_locals_data, valid_sigma, valid_globals_data);
+        write_to_file(file, output_buffer);
+        file.close();
+        auto reader = Binary{ Config{ .in_filename = file_name, .max_bufferpoint_size = 1U } };
+        auto init_err = reader.init();
+        EXPECT_TRUE(init_err);
+        auto read_err = reader.read_one_entry();
+        EXPECT_TRUE(read_err);
     }
 } // namespace centipede::test
