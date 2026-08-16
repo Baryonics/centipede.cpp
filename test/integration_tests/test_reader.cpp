48d5330c303779981d19d1e4c38620e5945cb4ac
Baryonics <benedikt.leicher@gmail.com>
2026-08-16 13:30:42 +0200

    temporary integration test adjustments

--- test/integration_tests/test_reader.cpp
+++ test/integration_tests/test_reader.cpp
@@ -23,24 +23,13 @@ auto main() -> int
         return EXIT_FAILURE;
     }
 
-    auto progress_adaptor = centipede::progress::ProgressAdaptor{
-        centipede::progress::config::BarWidth{ 50 },
-        centipede::progress::config::Start{ "[" },
-        centipede::progress::config::Fill{ "=" },
-        centipede::progress::config::Lead{ ">" },
-        centipede::progress::config::Remainder{ " " },
-        centipede::progress::config::End{ "]" },
-        centipede::progress::config::PostfixText{ "Reading binary data..." },
-        centipede::progress::config::ForegroundColor{ centipede::progress::ProgressColor::green },
-        centipede::progress::config::ShowPercentage{ true },
-        centipede::progress::config::FontStyles{
-            std::vector<centipede::progress::ProgressFontStyle>{ centipede::progress::ProgressFontStyle::bold } }
-    };
+    auto progress_indicator = centipede::ProgressIndicator{};
 
     std::size_t total_read{};
 
     for ([[maybe_unused]] const auto& entry :
-         reader | progress_adaptor(reader.get_file_size(), [&reader]() { return reader.get_last_entry_bytes(); }))
+         reader |
+             progress_indicator.adaptor(reader.get_file_size(), [&reader]() { return reader.get_last_entry_bytes(); }))
     {
         total_read += reader.get_last_entry_bytes();
     }
@@ -66,34 +55,34 @@ auto main() -> int
         return EXIT_FAILURE;
     }
 
-    if (const auto progress_adaptor_status = progress_adaptor.get_status();
-        progress_adaptor_status != centipede::ErrorCode::success)
-    {
-        std::println(stderr, "Error: {}", progress_adaptor_status);
-        return EXIT_FAILURE;
-    }
-
-    auto array = std::array{ 1, 2, 3, 4 };
-
-    auto progress_view = array | centipede::progress::ProgressAdaptor{};
-
-    for ([[maybe_unused]] auto elem : progress_view)
-    {
-    }
-
-    if (const auto progress_view_status = progress_view.get_status();
-        progress_view_status != centipede::ErrorCode::success)
-    {
-        std::println(stderr, "Error: {}", progress_view_status);
-        return EXIT_FAILURE;
-    }
-
-    if (const auto progress_adaptor_status = progress_adaptor.get_status();
-        progress_adaptor_status != centipede::ErrorCode::success)
-    {
-        std::println(stderr, "Error: {}", progress_adaptor_status);
-        return EXIT_FAILURE;
-    }
+    // if (const auto progress_adaptor_status = progress_adaptor.get_status();
+    //     progress_adaptor_status != centipede::ErrorCode::success)
+    // {
+    //     std::println(stderr, "Error: {}", progress_adaptor_status);
+    //     return EXIT_FAILURE;
+    // }
+    //
+    // auto array = std::array{ 1, 2, 3, 4 };
+    //
+    // auto progress_view = array | centipede::progress::ProgressAdaptor{};
+    //
+    // for ([[maybe_unused]] auto elem : progress_view)
+    // {
+    // }
+    //
+    // if (const auto progress_view_status = progress_view.get_status();
+    //     progress_view_status != centipede::ErrorCode::success)
+    // {
+    //     std::println(stderr, "Error: {}", progress_view_status);
+    //     return EXIT_FAILURE;
+    // }
+    //
+    // if (const auto progress_adaptor_status = progress_adaptor.get_status();
+    //     progress_adaptor_status != centipede::ErrorCode::success)
+    // {
+    //     std::println(stderr, "Error: {}", progress_adaptor_status);
+    //     return EXIT_FAILURE;
+    // }
 
     return EXIT_SUCCESS;
 }
